cb683dbf5a9c612a1e9f53f28e0cb95aac3961a7
M Pramodh <66459199+manduripramodh@users.noreply.github.com>
2020-08-18 11:36:37 +0530

    Update Explainabilty.py

--- steps/explanability.py
+++ steps/explanability.py
@@ -20,9 +20,21 @@ if __name__ == '__main__':
         clf_fn = pickle.load(f)
 
     feature_names = X_train.columns.tolist()
-    target_names_list = ['good', 'bad']
+    feature_importance_algorithm = "shap"
+    learning_type = os.environ['LEARNING_TYPE']
+    domain_type = os.environ['DOMAIN_TYPE']
+    model_interpretor_algorithm = os.environ['MODEL_INTERPRETOR_ALG']
+    model_explainer_algorithm = os.environ['MODEL_EXPLAINER_ALG']
+    target_names_list =['good', 'bad']
+    model_interpretor_strategy = os.environ['MODEL_INTERPRETOR_STRATEGY']
+    model_interpret_k_value = int(os.environ['MODEL_INTERPRETOR_K_VALUE']) 
+    pprint("Learning_type = " + learning_type)
+    pprint("Domain_type = " + domain_type)
+
+    model_interpret_top_value = 8
+    num_of_class = 2
     json_config = 'basic-report-explainable.json'
     controller = Controller(config=Configuration(json_config, locals()))
     pprint(controller.config)
     controller.render()
-    pprint("Compleyed explainer step")
+    pprint("Completed explainer step")
